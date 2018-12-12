44a52ea8e8d5a8190131d567680d0fd7fac2aa7a
Anthony CASANAVE <anthony.casanave@gmail.com>
2018-12-12 10:38:45 +0100

    "Ball" is moving all around the screen.
    Using while, if and elif to play with x and y so it bounce off the sides.

--- pong.py
+++ pong.py
@@ -1,12 +1,56 @@
+import time
 from sense_hat import SenseHat
-#Hissssss !
-
-
 
 sense = SenseHat()
 
-x=3
+x=1
 y=4
 
-sense.clear(0,0,0)
-sense.set_pixel(x,y,255,255,255)
+movex=True
+movey=True
+
+while True:
+  if movex is True and movey is True:
+    x+=1
+    y+=1
+    sense.clear(0,0,0)
+    sense.set_pixel(x,y,255,255,255)
+    time.sleep(0.1)
+    if x > 6:
+      movex=False
+    if y > 6:
+      movey=False
+
+  elif movex is True and movey is False:
+    x+=1
+    y-=1
+    sense.clear(0,0,0)
+    sense.set_pixel(x,y,255,255,255)
+    time.sleep(0.1)
+    if x > 6:
+      movex=False
+    if y < 1:
+      movey=True
+
+  elif movex is False and movey is False:
+    x-=1
+    y-=1
+    sense.clear(0,0,0)
+    sense.set_pixel(x,y,255,255,255)
+    time.sleep(0.1)
+    if x < 1:
+      movex=True
+    if y < 1:
+      movey=True
+
+  elif movex is False and movey is True:
+    x-=1
+    y+=1
+    sense.clear(0,0,0)
+    sense.set_pixel(x,y,255,255,255)
+    time.sleep(0.1)
+    if x < 1:
+      movex=True
+    if y > 6:
+      movey=False
+
